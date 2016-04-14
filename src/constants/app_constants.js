8eae70b2190912eade13344a58fec31ff2d0ba88
Brett Sun <qisheng.brett.sun@gmail.com>
2016-04-14 16:41:01 +0200

    Move base api and server urls into AppUrls

--- src/constants/app_constants.js
+++ src/constants/app_constants.js
@@ -1,9 +1,5 @@
-export const API_URL = process.env.API_URL;
 export const APP_BASE_PATH = process.env.APP_BASE_PATH;
-export const SERVER_URL = process.env.SERVER_URL;
 
 export default {
-    API_URL,
     APP_BASE_PATH,
-    SERVER_URL
 };
--- src/constants/app_urls.js
+++ src/constants/app_urls.js
@@ -1,17 +1,26 @@
-import { APP_BASE_PATH, SERVER_URL } from './app_constants';
+import { APP_BASE_PATH } from './app_constants';
+
+
+// Base urls from env
+export const BASE_API = process.env.API_URL;
+export const BASE_SERVER = process.env.SERVER_URL;
 
 // App urls
 export const APP_LOGIN = `${APP_BASE_PATH}login`;
 export const APP_SIGNUP = `${APP_BASE_PATH}signup`;
 
 // S3 urls
-export const S3_DELETE = `${SERVER_URL}/s3/delete/`;
-export const S3_KEY = `${SERVER_URL}/s3/key/`;
-export const S3_SIGNATURE = `${SERVER_URL}/s3/signature/`;
+export const S3_DELETE = `${BASE_SERVER}/s3/delete/`;
+export const S3_KEY = `${BASE_SERVER}/s3/key/`;
+export const S3_SIGNATURE = `${BASE_SERVER}/s3/signature/`;
 
 export default {
+    BASE_API,
+    BASE_SERVER,
+
     APP_LOGIN,
     APP_SIGNUP,
+
     S3_DELETE,
     S3_KEY,
     S3_SIGNATURE
