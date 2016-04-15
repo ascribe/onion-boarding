8c35926e09e993fd9143ef1cf5728438fefcb4bf
Brett Sun <qisheng.brett.sun@gmail.com>
2016-04-15 10:47:08 +0200

    Add sample popover helper

--- src/components/work_registration/work_registration_header.js
+++ src/components/work_registration/work_registration_header.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import CssModules from 'react-css-modules';
 
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import Popover from 'react-bootstrap/lib/Popover';
+
 import { getLangText } from '../../utils/lang';
 
 import styles from './work_registration_header.scss';
 
 
-const WorkRegistrationHeader = React.createClass({
-    onHelpToggle() {
-    },
+const WorkRegistrationHeader = () => {
+    const popover = (
+        <Popover id="help-text-popover" title={getLangText('Sample title')}>
+            {getLangText('Sample explainer text...')}
+        </Popover>
+    );
 
-    render() {
-        return (
-            <div styleName="container">
-                <h2 styleName="header-text">
-                    <div styleName="pretext">{getLangText('Generate a')}</div>
-                    <div styleName="emphasis">{getLangText('Certificate of Authenticity')}</div>
-                    <div styleName="subtext">{getLangText('Enter the details on the right')}</div>
-                </h2>
-                <button onClick={this.onHelpToggle} styleName="help-text-button">{getLangText('What is happening?')}</button>
-            </div>
-        );
-    }
-});
+    return (
+        <div styleName="container">
+            <h2 styleName="header-text">
+                <div styleName="pretext">{getLangText('Generate a')}</div>
+                <div styleName="emphasis">{getLangText('Certificate of Authenticity')}</div>
+                <div styleName="subtext">{getLangText('Enter the details on the right')}</div>
+            </h2>
+            <OverlayTrigger trigger="click" placement="top" overlay={popover}>
+                <button styleName="help-text-button">{getLangText('What is happening?')}</button>
+            </OverlayTrigger>
+        </div>
+    );
+};
 
 export default CssModules(WorkRegistrationHeader, styles);
